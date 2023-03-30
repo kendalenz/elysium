5dcb6a724d3e6da75ea6e1e94ad97ba94d88c897
Kendal Enz <kendal.enz@gmail.com>
2023-03-30 16:13:58 -0400

    test

--- server/app.js
+++ server/app.js
@@ -9,4 +9,4 @@ app.use('/static', express.static(path.join(__dirname, '../static')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
 
 module.exports = app; 
-// test
+// tes
