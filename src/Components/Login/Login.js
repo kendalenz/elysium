e9bd0483eab17c2b8b2f4f10af37e0e16fe11f94
Kendal Enz <kendal.enz@gmail.com>
2023-05-31 18:00:55 -0400

    working on login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { attemptLogin } from '../../store';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -8,7 +9,34 @@ const Login = () => {
     username: '',
     password: ''
   });
+
+  const [error, setError] = useState({});
+
+  const onChange = (ev) => {
+    setCredentials({ ...credentials, [ev.target.name]: ev.target.value });
+  };
+
+  const navigate = useNavigate();
   
+  const login = async(ev) => {
+    ev.preventDefault();
+    try {
+      await dispatch(attemptLogin(credentials));
+      navigate('/');
+    } catch(err) {
+        setError(err.response.data);
+    }
+  };
+
+  let messages = [];
+    if(error.errors) {
+      messages = error.errors.map((e) => e.message);
+    } else if(error.status) {
+      if (error.status === 401) {
+        messages.push('invalid credentials');
+     } else messages.push(error.status);
+  };
+
 }
 
 export default Login;
