3dadf609f9716881197f7eb7933b44c72ce3afd5
Kendal Enz <kendal.enz@gmail.com>
2023-05-31 11:13:55 -0400

    working on login

--- server/db/User.js
+++ server/db/User.js
@@ -1,5 +1,9 @@
 const { UUIDV4, STRING, UUID } = require('sequelize');
 const conn = require('./conn');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT;
+const axios = require('axios');
 
 const User = conn.define('user', {
   id: {
@@ -29,5 +33,45 @@ const User = conn.define('user', {
     unique: true
   }
 });
+
+User.addHook('beforeSave', async(user)=> {
+  if(user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, 5);
+  }
+});
+
+User.prototype.generateToken = function() {
+  return jwt.sign({ id: this.id }, JWT);
+};
+
+User.authenticate = async function({ email, password }) {
+  const user = await this.findOne({
+    where: {
+      email
+    }
+  });
+  if(user && await bcrypt.compare(password, user.password)) {
+    return jwt.sign({ id: user.id }, JWT);
+  }
+  const error = new Error('bad credentials');
+  error.status = 401;
+  throw error;
+};
+
+User.findByToken = async function(token) {
+  try {
+    const { id } = jwt.verify(token, process.env.JWT);
+    const user = await this.findByPK(id);
+    if(user) {
+      return user;
+    }
+    throw 'User not found';
+  }
+  catch(ex) {
+    const error = new Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+};
 
 module.exports = User;
